0eac1e40d61a68b536ec0b78290e4b38538aa3ca
RZRN3XUS <dycusc22@greenhill.org>
2020-04-27 15:13:48 -0500

    Fixing error messaage flow Servermain is now deprecated

--- src/create/clientMain.java
+++ src/create/clientMain.java
@@ -3,6 +3,7 @@ package create;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -11,34 +12,40 @@ class clientMain {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
-        Socket me = new Socket("52.91.76.77", 50); //Connecting to Server IP
-        //Greeting and version
-        System.out.println("Connected to server at " + me.getRemoteSocketAddress());
-        System.out.println("Welcome to the IMIP Client Version 1.2\n");
-        System.out.println("To logoff, type 'logoff' in your command prompt.");
-        System.out.print("Please enter a username: ");
-        String username = sc.next();
-        //Grabbing username for ID
-        DataOutputStream out = new DataOutputStream(me.getOutputStream());
-        DataInputStream in = new DataInputStream(me.getInputStream());
-        System.out.println("You have been connected to US CENTRAL IMIP Server at " + me.getRemoteSocketAddress());
-        out.writeUTF(username + " connected from " + me.getLocalSocketAddress());
-        AtomicBoolean detector = new AtomicBoolean(true); //Detector for IOException on 2nd Thread Init to kill program
-        Thread one = new Thread(() -> {
-            try {
-                Client.send(out, username); //starting client service to recieve inputs
-            } catch (IOException e) {
-                e.printStackTrace();
-                detector.set(false); //setting to false to stop program
+        Socket me;
+        try {
+            me = new Socket("192.168.2.229", 50); //Connecting to Server IP
+            System.out.println("Connected to server at " + me.getRemoteSocketAddress());
+            //Greeting and version
+            System.out.println("Welcome to the IMIP Client Version 1.2\n");
+            System.out.println("To logoff, type 'logoff' in your command prompt.");
+            System.out.print("Please enter a username: ");
+            String username = sc.next();
+            //Grabbing username for ID
+            DataOutputStream out = new DataOutputStream(me.getOutputStream());
+            DataInputStream in = new DataInputStream(me.getInputStream());
+            System.out.println("You have been connected to US CENTRAL IMIP Server at " + me.getRemoteSocketAddress());
+            out.writeUTF(username + " connected from " + me.getLocalSocketAddress());
+            AtomicBoolean detector = new AtomicBoolean(true); //Detector for IOException on 2nd Thread Init to kill program
+            Thread one = new Thread(() -> {
+                try {
+                    Client.send(out, username); //starting client service to recieve inputs
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    detector.set(false); //setting to false to stop program
+                }
+            });
+            if (detector.get()) {
+                Thread two = new Thread(() -> echoClient.echo(in)); //Recieving service to read messages
+                one.start(); //starting both threads to read/write simultaneously
+                two.start();
             }
-        });
-        if (detector.get()) {
-            Thread two = new Thread(() -> echoClient.echo(in)); //Recieving service to read messages
-            one.start(); //starting both threads to read/write simultaneously
-            two.start();
-        }
-        else {
-            System.err.println("System experienced an error while initializing echoClient."); //Stopping program if IO Error
+            else {
+                System.err.println("System experienced an error while initializing echoClient."); //Stopping program if IO Error
+                System.exit(00);
+            }
+        } catch (ConnectException e) {
+            System.err.println("Couldn't connect to server.");
             System.exit(00);
         }
     }
---
+++ src/create/testclass.java
@@ -0,0 +1,45 @@
+package create;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+class testclass {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        Socket me = new Socket("192.168.2.229", 50); //Connecting to Server IP
+        //Greeting and version
+        System.out.println("Connected to server at " + me.getRemoteSocketAddress());
+        System.out.println("Welcome to the IMIP Client Version 1.2\n");
+        System.out.println("To logoff, type 'logoff' in your command prompt.");
+        System.out.print("Please enter a username: ");
+        String username = sc.next();
+        //Grabbing username for ID
+        DataOutputStream out = new DataOutputStream(me.getOutputStream());
+        DataInputStream in = new DataInputStream(me.getInputStream());
+        System.out.println("You have been connected to US CENTRAL IMIP Server at " + me.getRemoteSocketAddress());
+        out.writeUTF(username + " connected from " + me.getLocalSocketAddress());
+        AtomicBoolean detector = new AtomicBoolean(true); //Detector for IOException on 2nd Thread Init to kill program
+        Thread one = new Thread(() -> {
+            try {
+                Client.send(out, username); //starting client service to recieve inputs
+            } catch (IOException e) {
+                e.printStackTrace();
+                detector.set(false); //setting to false to stop program
+            }
+        });
+        if (detector.get()) {
+            Thread two = new Thread(() -> echoClient.echo(in)); //Recieving service to read messages
+            one.start(); //starting both threads to read/write simultaneously
+            two.start();
+        }
+        else {
+            System.err.println("System experienced an error while initializing echoClient."); //Stopping program if IO Error
+            System.exit(00);
+        }
+    }
+}
